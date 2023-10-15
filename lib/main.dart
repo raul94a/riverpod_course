48a81675b88fb68f5f77df19b8482f6747b3bdf1
raul.albin <raul200794@gmail.com>
2023-10-15 19:31:16 +0200

    Fix folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod_course_preview/views/screens/login/login_screen.dart';
-import 'package:riverpod_course_preview/views/styles/colors.dart';
+import 'package:riverpod_course_preview/presentation/screens/login/login_screen.dart';
+import 'package:riverpod_course_preview/presentation/styles/colors.dart';
 
 void main() {
   runApp(const MainApp());
